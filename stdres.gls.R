e2abc8960aacb436ef0f76a4a5be1fa5402fa95a
Matthew Heaton <mheaton@MattHeatonMBP.local>
2019-02-19 09:55:38 -0700

    Created function for calculating decorrelated residuals

---
+++ stdres.gls.R
@@ -0,0 +1,47 @@
+stdres.gls <- function(glsobj){
+  
+  ## Dataset
+  data.char <- as.character(glsobj$call$data)
+  
+  ## If original model has a variance structure then construct the diagonal
+  ## matrices accordingly
+  if("varStruct"%in%names(glsobj$modelStruct)){
+    
+    Dinv <- varWeights(glsobj$modelStruct$varStruct)*(1/sigma(glsobj))
+    
+  } else {
+    
+    ## No variance model structure then weights are all 1
+    norig <- nrow(get(data.char))
+    Dinv <- rep(1,norig)*(1/sigma(glsobj))
+
+  } ## End if("varStruct"%in%names(glsobj$modelStruct))
+  
+  if("corStruct"%in%names(glsobj$modelStruct)){
+    
+    cor.pars <- coef(glsobj$modelStruct$corStruct,unconstrained=FALSE)
+    cor.call <- deparse(glsobj$call$correlation)
+    cor.call <- paste(substr(cor.call,1,nchar(cor.call)-1),", value = c(",
+                      paste(as.character(cor.pars),collapse=","),"),fixed=TRUE)")
+    R <- Initialize(eval(parse(text=cor.call)),data=get(data.char)) %>% corMatrix()
+    if(is.null(glsobj$groups) | length(unique(glsobj$group))==1){
+      decorr.resid <- as.numeric(solve(t(chol(R)))%*%glsobj$residuals)*Dinv
+    } else {
+      
+      resid.list <- base::split(residuals(glsobj), getGroups(glsobj))
+      decorr.resid <- sapply(1:length(resid.list), function(gind){
+        as.numeric(solve(t(chol(R[[gind]])))%*%resid.list[[gind]])
+      })
+      decorr.resid <- c(decorr.resid)*Dinv
+      
+    } ## End groups if statement
+    
+  } else {
+    
+    decorr.resid <- Dinv*glsobj$residuals
+    
+  } ## End if("corStruct"%in%names(glsobj$modelStruct))
+  
+  return(decorr.resid)
+  
+} #End stdres.gls function
